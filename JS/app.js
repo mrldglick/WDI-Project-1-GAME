79274aac1b905cf546418ee49ef4cb103a92dd6b
Louis Glick <mrldglick@gmail.com>
2018-07-16 17:58:07 +0100

    next turn debt interest and turn countdown complete, moving onto product random number refresh

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,12 @@
 $(() => {
+
+  ///////////////////VARIALBES///////////////
   const initialMoney = 2000;
   const initialHealth = 10;
   const initialTurns = 30;
   const initialDebtBalance = 4000;
   const interestRate = 0.1;
   const initialBankBalance = 0;
-  // etc...
-  //////////////////Character///////////////////////
-
-
   const $money = $('#money');
   const $health = $('#health');
   const $myPixieDust = $('#myPixieDust');
@@ -22,11 +20,11 @@ $(() => {
   const $bankBalance = $('#bankBalance');
   const $debtBalance = $('#debtBalance');
   const $payDebt = $('#payDebt');
+  const $nextTurn = $('#nextTurn');
+  // let turnsRemaining = initialTurns;
 
 
-
-
-
+  ////////////////////CHARACTER//////////////
   const character = {
 
     money: initialMoney,
@@ -42,30 +40,30 @@ $(() => {
     myGiantSpiderLeg: 0,
 
     bankBalance: initialBankBalance,
-    debtBalance: initialDebtBalance,
-
-
-    $payDebt.on('click', () => {
-      if(this.bankBalance >= this.debtBalance) {
-        this.bankBalance - this.debtBalance;
-        $debtBalance.html(character.debtBalance);
-        console.log('debt paid');
-      } else {
-        console.log('not enough money!');
-      }
-    }
+    debtBalance: initialDebtBalance
   };
-  // let turnsRemaining = initialTurns;
-  console.log('paying debt...');
-  character.payDebt();
+
+  $payDebt.on('click', () => {
+    console.log(character.bankBalance);
+    if(character.bankBalance >= character.debtBalance) {
+      character.bankBalance - character.debtBalance;
+      $debtBalance.html(character.debtBalance);
+      console.log('debt paid');
+    } else {
+      console.log('not enough money!');
+    }
+  });
+
+
 
   $money.html(`${character.money}`);
   $health.html(`${character.health}`);
   $myPixieDust.html(`${character.myPixieDust}`);
   $bankBalance.html(`${character.bankBalance}`);
   $debtBalance.html(`${character.debtBalance}`);
-  ///////////////////////BUY/SELL/ITEMS////////////////
 
+
+  ///////////////////////PRODUCTS////////////////
   ///////////////////////PIXIE/DUST////////////////////
   const $currentDisplayPricePixieDust = $('#currentPricePixieDust');
   const $currentDisplayAmountPixieDust = $('#amountAvailablePixieDust');
@@ -101,97 +99,31 @@ $(() => {
       alert('You cannot buy anymore! Sucks to be you.');
     }
   });
+  //////////////////TURN COUNTER & NEXT TURN/////
+
+  let turnCounter = initialTurns;
+  let $turnCounter = $('#turnCounter');
+
+  $nextTurn.on('click', () => {
+    if (turnCounter >= 1) {
+      console.log('you have 1+ turns');
+      turnCounter = (turnCounter - 1);
+      $turnCounter.html(turnCounter);
+      character.debtBalance = (character.debtBalance * 1.1);
+      $debtBalance.html(character.debtBalance);
+      
+
+
+    }
+  });
+
+
+
+
+
 
   console.log(pixieDust);
 
-  ///////////////////////DRAGONS/BLOOD////////////////////
-
-
-  //   const $currentDisplayDragonBlood = $('#currentPricedragonBlood');
-  //
-  //   let dragonBlood ={
-  //     currentPrice: 5,
-  //     amountAvailable: 10
-  //
-  //   };
-  //
-  //   $currentDisplaydragonBlood.html(`£${dragonBlood.currentPrice}`);
-  //
-  //   console.log(dragonBlood);
-  // ################################################
-  //
-  //   const $currentPriceDisplayPixieDust = $('#currentPricePixieDust');
-  //
-  //   let pixieDust ={
-  //     currentPrice: 5,
-  //     amountAvailable: 10
-  //
-  //   };
-  //
-  //   $currentPriceDisplayPixieDust.html(`£${pixieDust.currentPrice}`);
-  //
-  //   console.log(pixieDust);
-  //
-  //
-  //   const $currentPriceDisplayPixieDust = $('#currentPricePixieDust');
-  //
-  //   let pixieDust ={
-  //     currentPrice: 5,
-  //     amountAvailable: 10
-  //
-  //   };
-  //
-  //   $currentPriceDisplayPixieDust.html(`£${pixieDust.currentPrice}`);
-  //
-  //   console.log(pixieDust);
-  //
-  //   const $currentPriceDisplayPixieDust = $('#currentPricePixieDust');
-  //
-  //   let pixieDust ={
-  //     currentPrice: 5,
-  //     amountAvailable: 10
-  //
-  //   };
-  //
-  //   $currentPriceDisplayPixieDust.html(`£${pixieDust.currentPrice}`);
-  //
-  //   console.log(pixieDust);
-  //
-  //   const $currentPriceDisplayPixieDust = $('#currentPricePixieDust');
-  //
-  //   let pixieDust ={
-  //     currentPrice: 5,
-  //     amountAvailable: 10
-  //
-  //   };
-  //
-  //   $currentPriceDisplayPixieDust.html(`£${pixieDust.currentPrice}`);
-  //
-  //   console.log(pixieDust);
-  //
-  //   const $currentPriceDisplayPixieDust = $('#currentPricePixieDust');
-  //
-  //   let pixieDust ={
-  //     currentPrice: 5,
-  //     amountAvailable: 10
-  //
-  //   };
-  //
-  //   $currentPriceDisplayPixieDust.html(`£${pixieDust.currentPrice}`);
-  //
-  //   console.log(pixieDust);
-  //
-  //   const $currentPriceDisplayPixieDust = $('#currentPricePixieDust');
-  //
-  //   let pixieDust ={
-  //     currentPrice: 5,
-  //     amountAvailable: 10
-  //
-  //   };
-  //
-  //   $currentPriceDisplayPixieDust.html(`£${pixieDust.currentPrice}`);
-  //
-  //   console.log(pixieDust);
 
 
 });
