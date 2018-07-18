fe18246445c6b6b3dca65aeaed383384b21c0416
Louis Glick <mrldglick@gmail.com>
2018-07-18 11:55:08 +0100

    random encounter seems to be working and started css

--- JS/app.js
+++ JS/app.js
@@ -9,7 +9,7 @@ $(() => {
   const $nextTurn = $('#nextTurn');
   const $buttonDeposit = $('#buttonDeposit');
   const $inputDeposit = $('#inputDeposit');
-
+  const encounterChance = 0.05;
   const initialMoney = 2000;
   const initialHealth = 10;
   const initialTurns = 30;
@@ -154,6 +154,7 @@ $(() => {
         buyItem(item);
         console.log(character);
         displayCurrentItemMarket(item);
+        randomEncounter();
       } else {
         alert(`You cannot buy ${item.name}! Sucks to be you.`);
       }
@@ -188,6 +189,7 @@ $(() => {
         sellItem(item);
         console.log(character);
         displayCurrentItemMarket(item);
+        randomEncounter();
       } else {
         alert(`You cannot sell ${item.name}! You dont own any, idoit!`);
       }
@@ -209,6 +211,7 @@ $(() => {
       character.debtBalance =  (character.debtBalance * 1.1);
       $debtBalance.html((character.debtBalance).toFixed(2));
       allItems.forEach(item => item.recalculateMarket());
+      randomEncounter();
 
     } else if (character.debtBalance > 0) {
       alert('You loose! The goblin loan shark has cut your debt out of your flesh... fun times!');
@@ -218,7 +221,49 @@ $(() => {
   }
   //////////////////////Random Encounters//////////////////////////////////
 
-//create function around a random chance genrator (probably 'maths.random') which will if you health is greater than 1 subtract 1 health from character.health, and then create an alert to tell you that you have been attacked. If however you have health is equal to 1 it will send you an alert telling you that you have been attacked, died, and lost, then it will reset the window.
+  const encounters = [
+    { message: 'You got mugged by a fairy! You are pretty damn pathetic. -1 health, -25% of charcter money.'},
+    {message: 'You got attacked by a talking bunny! Its like you want to prove Darwin wrong. -1 health, -25% of charcter money.'},
+    {message: 'You tripped over a gnome and fell in a well. You are soggy and smell of frog poo. -1 health, -25% of charcter money.'},
+    {message: 'Stink from the sky!! A dragon has flown over you and decided your outfit could do with a paint job. You smell like a dragons outhouse. According to folklore this is lucky?  -1 health, -25% of charcter money.'},
+    {message: 'A beautiful elf woman leads you into an alley. You wake up sore and ashamed. -1 health, -25% of charcter money.'},
+    {message: 'A beautiful elf woman leads you into an alley. You wake up sore and ashamed. -1 health, -25% of charcter money.'}
+
+  ];
+
+  function randomEncounter(){
+    if (criticalFail()) {
+      const encounter = randomChoiceFromArray(encounters);
+      // Handle the encounter
+      alert(encounter.message);
+      // if (rollDice(encounter.healthLossRisk)) {
+      // lose health
+      character.health = character.health - 1;
+      character.money = character.money * 0.25;
+      $money.html((character.money).toFixed(2));
+      $health.html(`${character.health}`);
+      if (character.helth === 0) {
+        alert('You have died. Obviously this means you have lost.');
+        window.reset();
+      }
+    }
+    // then the same for money loss risk
+  }
+  // TODO: After the encounter, check for character health and maybe die!?
+
+
+  function criticalFail() {
+    return Math.random() < encounterChance;
+  }
+
+  function randomChoiceFromArray(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+
+  //create function around a random chance genrator (probably 'maths.random') which will if you health is greater than 1 subtract 1 health from character.health, and then create an alert to tell you that you have been attacked. If however you have health is equal to 1 it will send you an alert telling you that you have been attacked, died, and lost, then it will reset the window.
+
+  // place thisd function whithin the nextTurn functions first if statement, and first if statement for the clickListener of buy and sell
 
 
 });
